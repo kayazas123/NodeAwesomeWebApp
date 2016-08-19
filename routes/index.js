519966f77d72a25aa8a77ebf4dda584b6f12030f
Yochay Kiriaty <yochay@gmail.com>
2016-08-19 16:48:34 -0700

    mva git demo1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Awesome Web App in node js', text1:'AzureApp Service Roadshow' });
+  res.render('index', { title: 'Awesome Web App in node js', text1:'Azure App Service mva demo git deployment' });
 });
 
 module.exports = router;
