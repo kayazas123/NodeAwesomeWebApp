50785b4d0f48d1b2ea70eb34b66fd6409a489b3b
Yochay Kiriaty <yochay@gmail.com>
2017-01-19 10:09:58 -0800

    Chicago Tech Summit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Awesome Web App in node js', text1:'Azure App Service mva demo git deployment' });
+  res.render('index', { title: 'Awesome Web App in node js', text1:'Azure App Service Chicago Tech Summit demo git deployment' });
 });
 
 module.exports = router;
