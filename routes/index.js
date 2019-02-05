a31942cfa4743a70ad0a251774398364da134341
AHMED <ahmed@AHMEDs-MacBook-Pro.local>
2019-02-05 17:26:25 +0200

    Changed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Awesome Web App in node js', text1:'Azure App Service CHicago Tech Summit demo git deployment' });
+  res.render('index', { title: 'Awesome Web App in node js - Ayaz Ahmed Kuthus', text1:'Azure App Service CHicago Tech Summit demo git deployment' });
 });
 
 module.exports = router;
